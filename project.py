0b841a7ecddaef1a744b580f7bdb447b1dd1e738
Lenanetka <8882845+Lenanetka@users.noreply.github.com>
2025-05-28 01:33:25 +0200

    Read and write, learn and forget words

--- project.py
+++ project.py
@@ -1,18 +1,89 @@
+from word import Word
+
+words_unknown = []
+words_known = []
+
 def main():
-    ...
+    load_words()
+    print("Words loaded successfully.")
 
+    add_word("example")
+    learn_word("example")
+    forget_word("example")
+    delete_word("example")
 
-def function_1():
-    ...
+    print(f"Unknown words ({len(words_unknown)}):")
+    for word in words_unknown:
+        print(word)
+    print(f"Known words ({len(words_known)}):")
+    for word in words_known:
+        print(word)
 
+    input("Press Enter to save and quit...")
+    save_words()
 
-def function_2():
-    ...
+def load_words():
+    global words_known, words_unknown
+    words: list = Word.read_from_csv()
 
+    words_known = filter(lambda word: word.is_known, words)
+    words_known = sorted(words_known, key=lambda word: word.word)
+    words_known = sorted(words_known, key=lambda word: word.learned, reverse=True)
+    
+    words_unknown = filter(lambda word: not word.is_known, words)
+    words_unknown = sorted(words_unknown, key=lambda word: word.word)
+    words_unknown = sorted(words_unknown, key=lambda word: word.added, reverse=True)
 
-def function_n():
-    ...
+def save_words():
+    global words_known, words_unknown
+    words = words_unknown + words_known
+    Word.save_to_csv(words)
 
+def find_word_known(word: str) -> Word:
+    global words_known
+    for w in words_known:
+        if w.word == word:
+            return w
+    return None
+
+def find_word_unknown(word: str) -> Word:
+    global words_unknown
+    for w in words_unknown:
+        if w.word == word:
+            return w
+    return None
+
+def add_word(word: str):
+    if find_word_known(word) or find_word_unknown(word):
+        return
+    global words_unknown
+    w = Word(word)
+    words_unknown.insert(0, w)
+
+def learn_word(word: str):
+    global words_known, words_unknown
+    w = find_word_unknown(word)
+    if w is not None:
+        words_unknown.remove(w)
+        w.learn()
+        words_known.insert(0, w.learn())
+
+def forget_word(word: str):
+    global words_known, words_unknown
+    w = find_word_known(word)
+    if w is not None:
+        words_known.remove(w)
+        words_unknown.insert(0, w.forget())
+
+def delete_word(word: str):
+    global words_known, words_unknown
+    w = find_word_known(word)
+    if w is not None:
+        words_known.remove(w)
+    w = find_word_unknown(word)
+    if w is not None:
+        words_unknown.remove(w)
+        return
 
 if __name__ == "__main__":
     main()
---
+++ word.py
@@ -0,0 +1,97 @@
+from datetime import date
+import csv
+
+class Word:
+    WORDS_CSV = "words.csv"
+
+    def __init__(self, word, added=None, learned=None):
+        if isinstance(word, dict):
+            added = word["added"]
+            learned = word["learned"]
+            word = word["word"]
+        self.word = word
+        self.added = added
+        self.learned = learned
+
+    def __str__(self):
+        return f"Word: '{self.word}', added: {self.added}, learned: {self.learned}"
+    
+    def dictionary(self) -> dict:
+        return {
+            "word": self.word,
+            "added": self.added.isoformat() if self.added else None,
+            "learned": self.learned.isoformat() if self.learned else None
+        }
+    
+    def learn(self) -> 'Word':
+        if not self.is_known:
+            self.learned = date.today()
+        return self
+
+    def forget(self) -> 'Word':
+        if self.is_known:
+            self.learned = None
+        return self
+    
+    @property
+    def word(self):
+        return self._word
+    @word.setter
+    def word(self, word):
+        if word.strip() is None:
+            raise ValueError("Word cannot be None")
+        if not isinstance(word, str):
+            raise ValueError("Word must be a string")
+        if word.strip() == "":
+            raise ValueError("Word cannot be empty")
+        self._word = word.strip()
+
+    @property
+    def added(self):
+        return self._added
+    @added.setter
+    def added(self, added):
+        if added is None or (isinstance(added, str) and added.strip() == ""):
+            self._added = date.today()
+        else:
+            self._added = parse_date(added)
+        
+    @property
+    def learned(self):
+        return self._learned
+    @learned.setter
+    def learned(self, learned):
+        if learned is None or (isinstance(learned, str) and learned.strip() == ""):
+            self._learned = None
+        else:
+            self._learned = parse_date(learned)
+
+    @property
+    def is_known(self) -> bool:
+        return self.learned is not None
+
+    @classmethod
+    def read_from_csv(cls) -> list:
+        try:
+            with open(cls.WORDS_CSV, "r") as file:
+                words = csv.DictReader(file)
+                return [Word(word) for word in words]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_csv(cls, words: list):
+        words = [word.dictionary() for word in words]
+        with open(cls.WORDS_CSV, "w") as file:
+            writer = csv.DictWriter(file, fieldnames=["word", "added", "learned"])
+            writer.writeheader()
+            for word in words:
+                writer.writerow(word)
+
+def parse_date(d) -> date:
+    if isinstance(d, date):
+        return d
+    try:
+        return date.fromisoformat(d.strip())
+    except ValueError:
+        raise ValueError("Date must be in ISO format (YYYY-MM-DD)")
